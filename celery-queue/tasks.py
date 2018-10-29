7026cae30ec0fea061fdc48311768b7dfcde2a7e
Felipe Osório <felipe.osorio72@outlook.com.br>
2018-10-29 09:31:29 -0300

    [HOTFIX] Fix importer task

--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -10,7 +10,7 @@ CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
 CELERY_BEAT_SCHEDULE = {
-    'import': {
+    'import_data': {
         'task': 'tasks.import',
         'schedule': crontab('0', '1', '*', '*', '*')
     }
@@ -20,7 +20,7 @@ celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND
 celery.conf.beat_schedule = CELERY_BEAT_SCHEDULE
 
 @celery.task(name='tasks.import')
-def import():
+def import_data():
     print("Starting request")
     req = requests.get(os.environ['IMPORTER_GET'])
     requests.post(os.environ['CROSSDATA_POST'], json=req.json())
